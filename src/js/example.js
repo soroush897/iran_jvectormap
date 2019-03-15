9d7650997cd24974f1e15dac3e2570ddfd4545f3
soroush897 <soroush897@gmail.com>
2019-03-16 01:08:38 +0330

    Example For Run Iran All Cities and State Map for jVector Map Library

--- src/js/example.js
+++ src/js/example.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    jQuery('#iran_map').vectorMap({
-        map: 'iran_mill',
+    const jvm_map = $('#iran_map');
+    jvm_map.vectorMap({
+        map: 'ir_mill',
         enableZoom: false,
         backgroundColor: 'transparent',
         zoomOnScroll: true,
@@ -12,9 +13,43 @@ document.addEventListener("DOMContentLoaded", function (event) {
                 fill: '#DCE3E8',
                 "fill-opacity": 1,
                 stroke: '#fff',
-                "stroke-width": 1,
+                "stroke-width": 2,
                 "stroke-opacity": 1
             }
+        },
+        onRegionClick: function (event, code) {
+            if (code !== 'ir_kish') {
+                const jvm_container = jvm_map.children('.jvectormap-container');
+                jvm_container.addClass('ir_mill').hide();
+                $('.jvm_back').show();
+                setTimeout(function () {
+                    jvm_map.vectorMap({
+                        map: code,
+                        enableZoom: false,
+                        backgroundColor: 'transparent',
+                        zoomOnScroll: true,
+                        normalizeFunction: "polynomial",
+                        hoverOpacity: .7,
+                        hoverColor: false,
+                        regionStyle: {
+                            initial: {
+                                fill: '#DCE3E8',
+                                "fill-opacity": 1,
+                                stroke: '#fff',
+                                "stroke-width": 2,
+                                "stroke-opacity": 1
+                            }
+                        }
+                    });
+                }, 300);
+            } else {
+                event.stopImmediatePropagation();
+            }
         }
+    });
+    $('.jvm_back').click(function () {
+        $(this).hide();
+        $('.jvectormap-container:not(.ir_mill)').remove();
+        $('.jvectormap-container.ir_mill').removeClass('ir_mill').show();
     });
 });
